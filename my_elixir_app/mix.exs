b3ffb776f665d4e246134ded491ea205d13c393a
Pat Maddox <pat@patmaddox.com>
2022-10-29 15:04:16 -0700

    bump to 0.1.1

--- my_elixir_app/mix.exs
+++ my_elixir_app/mix.exs
@@ -4,7 +4,7 @@ defmodule MyElixirApp.MixProject do
   def project do
     [
       app: :my_elixir_app,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
